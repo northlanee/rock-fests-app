2dc186f72f349af07288da43b2747a523ea0b7d0
northlaneee <alex.yokai00@gmail.com>
2023-09-07 15:19:33 +0300

    dockerizing frontend

--- pages/api/fests/index.ts
+++ pages/api/fests/index.ts
@@ -1,15 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import fests from "./data.json";
-import { Fest } from "@/types";
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import fests from "./data.json";
+// import { Fest } from "@/types";
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Fest[] | { message: string }>
-) {
-  if (req.method === "GET") {
-    // res.status(200).json(fests);
-  } else {
-    res.status(405).json({ message: `Method ${req.method} is not allowed` });
-  }
-}
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Fest[] | { message: string }>
+// ) {
+//   if (req.method === "GET") {
+//     // res.status(200).json(fests);
+//   } else {
+//     res.status(405).json({ message: `Method ${req.method} is not allowed` });
+//   }
+// }
