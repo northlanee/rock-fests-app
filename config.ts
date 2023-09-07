74b8804fafe6cc8fdaf308f18e54137a403bddb5
northlaneee <alex.yokai00@gmail.com>
2023-09-07 16:46:25 +0300

    config

--- config.ts
+++ config.ts
@@ -2,8 +2,7 @@ export const API_URL =
   "https://rock-fests-app-backend-production.up.railway.app";
 // process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
 
-export const NEXT_URL =
-  "https://rock-fests-app-frontend-production.up.railway.app";
+export const NEXT_URL = "https://rock-fests.vercel.app/";
 // process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
 
 export const PER_PAGE = 2;
